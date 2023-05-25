2805b3f68b82ac9e18ed5635c0c25fa2dd823c6d
Shaf8808 <shadmankhan08@gmail.com>
2023-05-25 10:20:24 +0100

    Add senMail function that takes in the values of the users' name, email address and project request, then sends an email to me

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("Hiiiiiiiiiiiiii")
+function sendMail(contactForm) {
+    emailjs.send("service_5ox3c7p","Shadman", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response)
+        },
+        function(error) {
+            console.log("FAILED", error)
+        })
+    return false
+}
